724fab3560a1f2af97d811dd5ce8e260cb33df9c
Srinidhi Yoganand <120911846+Srinidhi-Yoganand@users.noreply.github.com>
2024-11-17 16:06:37 +0530

    configured build.gradle file and created entities

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,13 @@
  */
 package org.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
