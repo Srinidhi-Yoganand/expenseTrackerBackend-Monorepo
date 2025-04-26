974d8cdf2eabea7b97c3ab7f810bff352044d446
Srinidhi Yoganand <120911846+Srinidhi-Yoganand@users.noreply.github.com>
2025-04-26 17:20:43 +0530

    feat: changed the api endpoint

--- parserService/expenses/urls.py
+++ parserService/expenses/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import process_message_view
 
 urlpatterns = [
-  path("process-message/", process_message_view, name="process_message"),
+  path("v1/ds/message", process_message_view, name="process_message"),
 ]
