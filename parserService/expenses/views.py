e5ed9cf9ebeecc0dc78caa22fded37bde32905fa
Srinidhi Yoganand <120911846+Srinidhi-Yoganand@users.noreply.github.com>
2025-04-26 17:13:53 +0530

    feat: created view

--- parserService/expenses/views.py
+++ parserService/expenses/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .message_service import MessageService
 
-# Create your views here.
+@csrf_exempt
+def process_message_view(request):
+  if request.method == 'POST':
+    data = json.loads(request.body)
+    message = data.get("message", "")
+    service = MessageService()
+    expense = service.process_message(message)
+    if expense:
+      return JsonResponse(expense.serialize())
+    else:
+      return JsonResponse({"error": "Not a bank message"}, status=400)
+  return JsonResponse({"error": "Invalid method"}, status=405)
